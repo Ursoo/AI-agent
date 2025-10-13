0a96e5cbf1f58ddd3e74d70868af063e96ca8b15
Ursoo <ursu.calin18@gmail.com>
2025-10-13 23:39:44 +0300

    Minor update playing around with system prompts

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 MAX_READ_CHARS = 10000
+SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from config import SYSTEM_PROMPT
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -17,7 +19,7 @@ messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
 def run():
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages)
+        model="gemini-2.0-flash-001", contents=messages, config= types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT))
     return response
 
 
